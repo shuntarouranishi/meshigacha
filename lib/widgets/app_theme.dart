ba96913d92a699561889ad88dc9e127119d6da3b
shuntarouranishi <shuntaro.uranishi@gmail.com>
2023-08-05 15:23:42 +0900

    テーマのエラー解除

--- lib/widgets/app_theme.dart
+++ lib/widgets/app_theme.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
   primarySwatch: Colors.brown,
-  accentColor: Colors.orangeAccent,
   textTheme: TextTheme(
     headline1: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
     headline2: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
@@ -10,9 +9,6 @@ final ThemeData appTheme = ThemeData(
   ),
   appBarTheme: AppBarTheme(
     centerTitle: true,
-    textTheme: TextTheme(
-      headline6: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-    ),
   ),
   elevatedButtonTheme: ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
